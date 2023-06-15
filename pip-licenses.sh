1252fab0d811882ed4fd6f673761075ae8f9e3db
Greg Sheppard <13774377+gregalia@users.noreply.github.com>
2023-06-15 19:36:31 +0300

    Run script with basenamed env var

--- pip-licenses.sh
+++ pip-licenses.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 
-# echo "create requirements environment"
-# python3 -m venv requirements-env
-# source requirements-env/bin/activate
-# pip install -r requirements.txt
-# deactivate
-#
-# echo "create pip-licenses environment"
-# python3 -m venv pip-licenses-env
-# source pip-licenses-env/bin/activate
-# pip install pip-licenses
+echo "create requirements environment"
+python3 -m venv requirements-env
+source requirements-env/bin/activate
+pip install -r requirements.txt
+deactivate
 
-echo "${1}"
+echo "create pip-licenses environment"
+python3 -m venv pip-licenses-env
+source pip-licenses-env/bin/activate
+pip install pip-licenses
 
-# LICENSE_FILE="${1}"
-#
-# {
-#   echo -e "# ${GITHUB_REPOSITORY} Licenses\n"
-#
-#   pip-licenses \
-#     --python=./requirements-env/bin/python3 \
-#     --from=mixed \
-#     --order=license \
-#     --format=markdown
-#
-#   echo -e "\n## Summary\n"
-#
-#   pip-licenses \
-#     --python=./requirements-env/bin/python3 \
-#     --from=mixed \
-#     --order=license \
-#     --format=markdown \
-#     --summary
-#
-# } >>"${LICENSE_FILE}"
-#
-# cat "${LICENSE_FILE}"
+LICENSE_FILE="${1}"
+
+{
+  echo -e "# ${GITHUB_REPOSITORY} Licenses\n"
+
+  pip-licenses \
+    --python=./requirements-env/bin/python3 \
+    --from=mixed \
+    --order=license \
+    --format=markdown
+
+  echo -e "\n## Summary\n"
+
+  pip-licenses \
+    --python=./requirements-env/bin/python3 \
+    --from=mixed \
+    --order=license \
+    --format=markdown \
+    --summary
+
+} >>"${LICENSE_FILE}"
+
+cat "${LICENSE_FILE}"
