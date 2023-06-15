dbf94bd483bac43b326f6da51d37b375cf1eb1dc
Greg Sheppard <13774377+gregalia@users.noreply.github.com>
2023-06-15 17:00:17 +0300

    Run via script

--- pip-licenses.sh
+++ pip-licenses.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 
-echo "hello world!"
+echo "create requirements environment"
+python3 -m venv requirements-env
+source requirements-env/bin/activate
+pip install -r requirements.txt
+deactivate
+
+echo "create pip-licenses environment"
+python3 -m venv pip-licenses-env
+source pip-licenses-env/bin/activate
+pip install pip-licenses
+
+echo "see if firetail shows up"
+pip list -v |
+  grep site-packages
+
+echo "run as-is"
+pip-licenses
+echo "run with python from requirements env"
+pip-licenses \
+  --python=./requirements-env/bin/python3
